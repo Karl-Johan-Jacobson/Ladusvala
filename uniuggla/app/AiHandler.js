13fc47cd7f9fc43793a40c130dbd79b1ab783688
Karljohan Wejnefalk Jakobsson <karljohanwejnefalkjakobsson@n148-p253.eduroam.kth.se>
2024-04-16 08:31:24 +0200

    Task 3.0

---
+++ uniuggla/app/AiHandler.js
@@ -0,0 +1,20 @@
+import OpenAI from "openai";
+
+
+
+// DONT Commit Change API key so it is not visible
+
+
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+}
+main();
