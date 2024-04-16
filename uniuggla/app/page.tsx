4ee7e5399fcac71d1808898271b77f5fbab3ee93
Victork02 <victor.j.karlstrom@gmail.com>
2024-04-16 11:14:42 +0200

    Added initial structure and functionality for interest list

--- uniuggla/app/page.tsx
+++ uniuggla/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            hej
+            Find in-depth information about Next.js features and API.
           </p>
         </a>
 
