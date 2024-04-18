9818fa713a38550159e8cb9177498c47275ee24a
Victork02 <victor.j.karlstrom@gmail.com>
2024-04-18 16:09:24 +0200

    Removed unused/unnecessary files

--- uniuggla/app/program_recommendations/buttonLogic.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleButtonClick = void 0;
-var handleButtonClick = function (userInput) {
-    return userInput + "\n";
-};
-exports.handleButtonClick = handleButtonClick;
