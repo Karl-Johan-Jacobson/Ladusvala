5e643f9ac0f453e42bf789912ecb0cd262552443
RasmusSjo <rasmus.sjoberg15@gmail.com>
2024-04-18 16:30:31 +0200

    Selected interests are now highlighted

--- uniuggla/components/client/custom_interest_list_item.tsx
+++ uniuggla/components/client/custom_interest_list_item.tsx
@@ -8,23 +8,23 @@ interface CustomInterestListItemProps {
   onToggle: (isToggled: boolean, id: string) => void;
 }
 
-export default function CustomInterestListItem({ interest, onToggle }: CustomInterestListItemProps) {
-    const [isToggled, setIsToggled] = useState<boolean>(true);
-  
-    function toggle() {
-      const newState = !isToggled;
-      setIsToggled(newState);
-      onToggle(newState, interest.interestId);
-    }
-  
-    return (
-      <div>
-        <button
-          className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-3xl text-sm px-5 py-2.5 text-center me-2 mb-2"
-          onClick={toggle}>
-          {interest.interestTitle}
-        </button>
-      </div>
-    );
+export default function CustomInterestListItem({
+  interest,
+  onToggle,
+}: CustomInterestListItemProps) {
+  const [isToggled, setIsToggled] = useState<boolean>(true);
+
+  function toggle() {
+    const newState = !isToggled;
+    setIsToggled(newState);
+    onToggle(newState, interest.interestId);
   }
-  
+
+  return (
+    <div>
+      <button className="interestSelected" onClick={toggle}>
+        {interest.interestTitle}
+      </button>
+    </div>
+  );
+}
--- uniuggla/components/client/interest_list_item.tsx
+++ uniuggla/components/client/interest_list_item.tsx
@@ -12,18 +12,25 @@ export default function InterestListItem({ interest, onToggle }: InterestListIte
   const [isToggled, setIsToggled] = useState<boolean>(false);
 
   function toggle() {
-    setIsToggled(!isToggled)
-    onToggle(isToggled, interest.interestId)
+    setIsToggled(!isToggled);
+    onToggle(isToggled, interest.interestId);
   }
 
   return (
     <div>
-      {!isToggled && <button className="text-white bg-gradient-to-r from-purple-300 via-purple-400 to-purple-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-3xl text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={() => (toggle())}>
-        {interest.interestTitle}
-      </button>}
-      {isToggled && <button className="text-wh  ite bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-3xl text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={() => (toggle())}>
-        {interest.interestTitle}
-      </button>}  
+      {!isToggled && (
+        <button
+          className="text-white bg-gradient-to-r from-purple-300 via-purple-400 to-purple-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-3xl text-sm px-5 py-2.5 text-center me-2 mb-2"
+          onClick={() => toggle()}
+        >
+          {interest.interestTitle}
+        </button>
+      )}
+      {isToggled && (
+        <button onClick={() => toggle()} className="interestSelected">
+          {interest.interestTitle}
+        </button>
+      )}
     </div>
   );
 }
