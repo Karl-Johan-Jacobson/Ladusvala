4b0690b4843506dc57e096a799e0ecec80950412
RasmusSjo <rasmus.sjoberg15@gmail.com>
2024-04-18 16:32:25 +0200

    The logo and "Hitta program" now links to the homepage

--- uniuggla/components/server/footer.tsx
+++ uniuggla/components/server/footer.tsx
@@ -1,22 +1,38 @@
 import Link from "next/link";
 
 export default function Footer() {
-	return (
-		<footer>
-			<div className="footerLeft">
-				<div className="footerLine"></div>
-				<div className="footerListDiv">
-					<ul className="footerList">
-						<li><Link href="/page" className="siteNavigation">Hitta program</Link></li>
-						<li><Link href="/count" className="siteNavigation">Räkna antagningspoäng</Link></li>
-						<li><Link href="/req" className="siteNavigation">Se behörighetskrav</Link></li>
-						<li><Link href="/about/" className="siteNavigation">Om oss</Link></li>
-					</ul>
-				</div>
-			</div>
-			<div className="footerLogoDiv">
-				<img src="../../text_logo.png" alt="Logo_Text"/>
-			</div>
-		</footer>
-	);
+  return (
+    <footer>
+      <div className="footerLeft">
+        <div className="footerLine"></div>
+        <div className="footerListDiv">
+          <ul className="footerList">
+            <li>
+              <Link href="/" className="siteNavigation">
+                Hitta program
+              </Link>
+            </li>
+            <li>
+              <Link href="/count" className="siteNavigation">
+                Räkna antagningspoäng
+              </Link>
+            </li>
+            <li>
+              <Link href="/req" className="siteNavigation">
+                Se behörighetskrav
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="siteNavigation">
+                Om oss
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="footerLogoDiv">
+        <img src="../../text_logo.png" alt="Logo_Text" />
+      </div>
+    </footer>
+  );
 }
--- uniuggla/components/server/header.tsx
+++ uniuggla/components/server/header.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
-	return (
-		<header>
-			<img src="../../uniu_logo_filled.svg" alt="Logo"/> 
-		</header>
-	);
+  return (
+    <header>
+      <Link href="/">
+        <img src="../../uniu_logo_filled.svg" alt="Logo" />
+      </Link>
+    </header>
+  );
 }
