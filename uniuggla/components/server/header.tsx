5c32aaeeb81e8787f9a86e036fbe6ef8d0381be9
Karl-Johan-Jacobson <karljohanwj@outlook.com>
2024-04-23 11:32:13 +0200

    Change <link> to <a> in header and footer.

--- uniuggla/components/server/header.tsx
+++ uniuggla/components/server/header.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header>
-      <Link href="/">
+      <a href="/">
         <img src="../../uniu_logo_filled.svg" alt="Logo" />
-      </Link>
+      </a>
     </header>
   );
 }
