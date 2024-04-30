ad5b20c28a6aeeb80790eba3354f7956945f1c7d
Karl-Johan-Jacobson <karljohanwj@outlook.com>
2024-04-30 08:38:19 +0200

    close #60 fixed unified inputfields
    Co-authored-by: Fredrik B <fredrikberzins@users.noreply.github.com>

--- uniuggla/components/client/Recommendation.tsx
+++ uniuggla/components/client/Recommendation.tsx
@@ -8,37 +8,31 @@ import { NextPage } from "next";
 import recommendProgramFromInterest from "@/ai/AiHandler";
 
 const Recommendation: React.FC<RecommendationProps & {}> = () => {
-  //HTML code
-  return (
-    <div
-      id="recommmendation"
-      className="wrapper recommmendationWrapper recommmendation"
-    >
-      <p
-        className="bot titleTypewriter recommmendationText"
-        style={{ paddingTop: "10vh" }}
-      ></p>
-      <div className="filterWrapper">
-        <form className="textInputWrapper" action="">
-          <input className="textInput" type="text" placeholder="Lärosäte" />
-          <button className="textButton">
-            <img className="textButtonImg" src="../../three_dot.svg" alt="" />
-          </button>
-        </form>
-        <form className="textInputWrapper" action="">
-          <input className="textInput" type="text" placeholder="År" />
-          <button className="textButton">
-            <img className="textButtonImg" src="../../three_dot.svg" alt="" />
-          </button>
-        </form>
-      </div>
-      <div className="recommendedWrapper">
-        <div className="aiAnswer">
-          <p>Loading...</p>
-        </div>
-      </div>
-    </div>
-  );
+	//HTML code
+	return (
+		<div id="recommmendation" className="wrapper recommmendationWrapper recommmendation">
+			<p className="bot titleTypewriter recommmendationText" style={{ paddingTop: "10vh" }}></p>
+			<div className="filterWrapper">
+				<form className="textInputWrapper" action="">
+					<input className="textInput" type="text" placeholder="Lärosäte" />
+					<button className="textButton">
+						<img className="textButtonImg" src="../../three_dot.svg" alt="" />
+					</button>
+				</form>
+				<form className="textInputWrapper" action="">
+					<input className="textInput" type="text" placeholder="År" />
+					<button className="textButton">
+						<img className="textButtonImg" src="../../three_dot.svg" alt="" />
+					</button>
+				</form>
+			</div>
+			<div className="recommendedWrapper">
+				<div className="loadingGIF">
+					<img src="../../uniugglan.gif" alt="Waiting on recomendation." />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Recommendation;
