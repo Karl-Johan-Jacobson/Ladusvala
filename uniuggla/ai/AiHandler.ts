7fbfd55d49e40fc5745b2fdf507095671d601f04
fredrikberzins <fredrik.berzins@gmail.com>
2024-05-02 14:23:18 +0200

    remmoved duplicate of shuffleArray

--- uniuggla/ai/AiHandler.ts
+++ uniuggla/ai/AiHandler.ts
@@ -190,9 +190,3 @@ function shuffleArray<T>(array: T[]): void {
     [array[i], array[j]] = [array[j], array[i]];
   }
 }
-function shuffleArray<T>(array: T[]): void {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
