ea73af14d96cccc773b8dd572fd7b55db69d0165
Ptolemy <love.mitteregger@proton.me>
2024-05-14 16:01:29 +0200

    updated about us page

--- uniuggla/app/about/page.tsx
+++ uniuggla/app/about/page.tsx
@@ -3,17 +3,17 @@ export default function About() {
 	return (
 		<main>
 			<div className="wrapper">
-				<div className="title" style={{ marginTop: "12vh" }}>
-					<p className="titleAboutUs">OM UNIU</p>
-
-					<div className="body" style={{ marginTop: "4vh" }}>
-						<p>UNIU är din AI-baserade studievägledare som kan hjälpa dig <br></br> att hitta din drömutbildning baserat på dina intressen. <br></br> Vi baserar våra svar på majoriteten av alla universitets-/högskoleutbildningar.</p>
+				<div className="body" style={{ marginTop: "18vh" }}>
+					<div className="user" style={{ marginTop: "2vh" }}>
+						<p>UNIU är Europas första AI-baserade studievägledare som kan hjälpa dig att hitta din drömutbildning baserat på dina intressen.<br></br> De utbildningar som UNIU rekommenderar är hämtade från majoriteten av alla universitets-/högskoleutbildningar i Sverige.<br></br>Matchningen sker med hjälp av ChatGPT 3.5 Turbo från OpenAI.</p>
 					</div>
-					<div className="body" style={{ marginTop: "3vh" }}>
-						<p>Hemsidan har skapats av 8st KTH-studenter för kursen II1305, <br></br>Projekt inom informations- och kommunikationsteknik, under våren 2024.</p>
+					<div className="user" style={{ marginTop: "2vh" }}>
+						<p>UNIU skapades av åtta KTH-studenter för kursen II1305, Projekt inom informations- och kommunikationsteknik, under våren 2024.<br></br>Utvecklingen av hemsidan pågick under fyra arbetsveckor och applicerade Scrum som samarbetesramverk.<br></br>
+					   Mer information om projektet kan läsas här: <a className="aboutUsLink" href="https://ladusvala2024.wixsite.com/teamladusvala"> Team Ladusvala </a></p>
 					</div>
-					<div className="body" style={{ marginTop: "3vh" }}>
-						<p>Utvecklingen av hemsidan fortlöpte under Scrum ramverket med sprintar som motsvarade 4st arbetsveckor. <br></br> Mer information om projektet kan läsas här: <a className="aboutUsLink" href="https://ladusvala2024.wixsite.com/teamladusvala"> Team Ladusvala </a></p>
+					<div className="user" style={{ marginTop: "2vh" }}>
+						<p>Hemsidan UNIU.nu är primärt byggd i TypeScript-kod och applicerar Next.js som React-ramverk, hosting sker via Vercel.
+							<br></br>Designen av hemsidan framställdes via Figma.</p>
 					</div>
 				</div>
 				<div className="body" style={{margin: "0 auto", textAlign: "left", width: "max-content", display: "block"}}>
@@ -28,10 +28,27 @@ export default function About() {
 						<p style={{ marginTop: "0.5vh"}}> Victor Karlström </p>
 				</div>
 				<div className="body">
-					<p className="user" style={{ marginTop: "2.5vh"}}> Den applicerade AI-modellen är OpenAI:s ChatGPT 3.5 Turbo </p>
 					<p className="user" style={{ marginTop: "1.5vh"}}> Information om utbildningsprogrammen är hämtat från respektive utbildningsprograms lärosäte. </p>
 					<p className="user" style={{ marginTop: "1.5vh"}}> Jämförelsetalsräknaren är baserad på den metod för uträkning av jämförelsetal som Antagning.se applicerar</p>
-					<p className="user" style={{ marginTop: "1.5vh"}}> Copyright: </p>
+					<p className="user" style={{ marginTop: "1.5vh"}}> 
+<br></br>
+<br></br>
+COPYRIGHT 2024 TEAM LADUSVALA*
+<br></br>
+<br></br>
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+<br></br>
+<br></br>
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+<br></br>
+<br></br>
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+<br></br>
+<br></br>
+</p>
+<p className="user" style={{ marginTop: "1.5vh"}}> 
+*Team Ladusvala consists of:
+Elias Gaghlasian, Fredrik Berzins, Karl-Johan Jakobsson Wejnefalk, Love Mitteregger, Naveed Rahman, Oscar Gertling, Rasmus Sjöberg, Victor Karlström</p>
 				</div>
 			</div>
 		</main>
