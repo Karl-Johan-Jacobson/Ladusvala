9aff64a5435777f491772176631b22365bf97ba2
RasmusSjo <rasmus.sjoberg15@gmail.com>
2024-05-14 16:35:59 +0200

    Changed routing to updated point counter page

--- uniuggla/components/client/HamburgerNavigationBar.tsx
+++ uniuggla/components/client/HamburgerNavigationBar.tsx
@@ -1,26 +1,40 @@
 // components/HamburgerNavigationBar.tsx
 
-import React from 'react';
+import React from "react";
 
 // Define the interface for the component props
 interface HamburgerNavigationBarProps {
   onToggle: () => void; // Function that doesn't take parameters and returns void
-  isOpen: boolean;     // Boolean state
+  isOpen: boolean; // Boolean state
 }
 
-const HamburgerNavigationBar: React.FC<HamburgerNavigationBarProps> = ({ onToggle, isOpen }) => {
+const HamburgerNavigationBar: React.FC<HamburgerNavigationBarProps> = ({
+  onToggle,
+  isOpen,
+}) => {
   const toggleDropdown = () => {
     onToggle(); // Call the function passed from the parent component
   };
 
   return (
     <nav className="hamburger-nav">
-      <img className="hamburger-icon" src="../../hamburger.svg" alt="" onClick={toggleDropdown}/>
+      <img
+        className="hamburger-icon"
+        src="../../hamburger.svg"
+        alt=""
+        onClick={toggleDropdown}
+      />
       {isOpen && (
         <ul className="dropdown-menu">
-          <li><a href="/">Hitta program</a></li>
-          <li><a href="/count">Beräkna antagningspoäng</a></li>
-          <li><a href="/about">Om oss</a></li>
+          <li>
+            <a href="/">Hitta program</a>
+          </li>
+          <li>
+            <a href="/berakna-poang">Beräkna antagningspoäng</a>
+          </li>
+          <li>
+            <a href="/about">Om oss</a>
+          </li>
         </ul>
       )}
     </nav>
